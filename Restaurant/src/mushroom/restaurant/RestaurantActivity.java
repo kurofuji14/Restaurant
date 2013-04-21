22348ee1db7cc01e0e1fd0abe49c25de81bf3910
mushroom <kurofuji14@gmail.com>
2013-04-21 17:24:54 +0900

    new create

---
+++ Restaurant/src/mushroom/restaurant/RestaurantActivity.java
@@ -0,0 +1,13 @@
+package mushroom.restaurant;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RestaurantActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
